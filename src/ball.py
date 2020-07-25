0f23e31068ef5b7694f34dde4ca8c23b86372bca
Tomasz Wojdat <trivvz3@gmail.com>
2020-07-25 17:08:16 +0200

    Add ball coordinates and constants for color and size

--- src/ball.py
+++ src/ball.py
@@ -1,3 +1,12 @@
+BALL_COLOR = "black"
+BALL_SIZE = 5
+
+
 class Ball:
-    color = "black"
-    size = 5
+    color = BALL_COLOR
+    size = BALL_SIZE
+
+    def __init__(self, x: int = 0, y: int = 0):
+        self.x = x
+        self.y = y
+
--- tests/test_ball.py
+++ tests/test_ball.py
@@ -1,9 +1,21 @@
 import unittest
 
-from src.ball import Ball
+from src.ball import Ball, BALL_SIZE, BALL_COLOR
 
 
 class TestBall(unittest.TestCase):
-    def test_ball(self):
+    def test_ball_x(self):
+        ball = Ball(10, 20)
+        self.assertEqual(ball.x, 10)
+
+    def test_ball_y(self):
+        ball = Ball(10, 20)
+        self.assertEqual(ball.y, 20)
+
+    def test_ball_color(self):
         ball = Ball()
-        self.assertTrue(True)
+        self.assertEqual(ball.color, BALL_COLOR)
+
+    def test_ball_size(self):
+        ball = Ball()
+        self.assertEqual(ball.size, BALL_SIZE)
