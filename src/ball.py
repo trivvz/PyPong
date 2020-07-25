2e0bbc69223441b55117a38280364a54331c18a4
Tomasz Wojdat <trivvz3@gmail.com>
2020-07-25 19:57:42 +0200

    Change Ball.pos and Ball.speed to np.ndarray types. Add draw and update methods
    Update tests accordingly
    Add requirements.txt

--- src/ball.py
+++ src/ball.py
@@ -1,12 +1,22 @@
-BALL_COLOR = "black"
+from typing import Tuple
+
+import  pygame
+import numpy as np
+
+BALL_COLOR = (255, 255, 255)
 BALL_SIZE = 5
 
 
 class Ball:
     color = BALL_COLOR
-    size = BALL_SIZE
+    radius = BALL_SIZE
 
-    def __init__(self, x: int = 0, y: int = 0):
-        self.x = x
-        self.y = y
+    def __init__(self, pos: Tuple[int, int] = (0, 0), speed: Tuple[int, int] = (1, 1)):
+        self.pos = np.array(pos)
+        self.speed = np.array(speed)
 
+    def draw(self, screen) -> None:
+        pygame.draw.circle(screen, self.color, self.pos, self.radius)
+
+    def update(self) -> None:
+        self.pos += self.speed
--- tests/test_ball.py
+++ tests/test_ball.py
@@ -1,16 +1,26 @@
 import unittest
 
+import numpy as np
+
 from src import ball
 
 
 class TestBall(unittest.TestCase):
-    def test_ball_x(self):
-        test_ball = ball.Ball(10, 20)
-        self.assertEqual(test_ball.x, 10)
+    def test_ball_pos_x(self):
+        test_ball = ball.Ball((10, 20))
+        self.assertEqual(test_ball.pos[0], 10)
 
-    def test_ball_y(self):
-        test_ball = ball.Ball(10, 20)
-        self.assertEqual(test_ball.y, 20)
+    def test_ball_pos_y(self):
+        test_ball = ball.Ball((10, 20))
+        self.assertEqual(test_ball.pos[1], 20)
+
+    def test_ball_speed_x(self):
+        test_ball = ball.Ball((10, 20), (50, 60))
+        self.assertEqual(test_ball.speed[0], 50)
+
+    def test_ball_speed_y(self):
+        test_ball = ball.Ball((10, 20), (50, 60))
+        self.assertEqual(test_ball.speed[1], 60)
 
     def test_ball_color(self):
         test_ball = ball.Ball()
@@ -18,4 +28,10 @@ class TestBall(unittest.TestCase):
 
     def test_ball_size(self):
         test_ball = ball.Ball()
-        self.assertEqual(test_ball.size, ball.BALL_SIZE)
+        self.assertEqual(test_ball.radius, ball.BALL_SIZE)
+
+    def test_ball_update(self):
+        test_ball = ball.Ball((25, 75), (5, -5))
+        test_ball.update()
+        self.assertEqual(test_ball.pos[0], 30)
+        self.assertEqual(test_ball.pos[1], 70)
